77a7c7278d1824110262b5d187033409f6df17a3
Ashim Bhatta <45790392+ashimbhatta1@users.noreply.github.com>
2019-05-06 17:12:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,41 @@
-var isMenuShown = false;
-function sideNav(){
-	if (isMenuShown === false) {
-		document.getElementById('list').style.display = 'flex'
-		document.getElementById('line3').style.display="none"
-		document.getElementById('line1').style.transform= "rotate(45deg)"
-		document.getElementById('line2').style.transform= "rotate(-45deg)"
-		document.getElementById('line1').style.marginButtom= "0"
-		document.getElementById('line2').style.marginTop= "-6px"
-		document.getElementById('line2').style.transition= "0.3s"
-		document.getElementById('line1').style.transition= "0.3s"
-		document.getElementById('line1').style.backgroundColor= "green"
-		document.getElementById('line2').style.backgroundColor= "green"
-		isMenuShown = true
+var isMenuhide = true;
+var sideMenu = document.getElementById('sideMenu')
+
+
+
+function toggleOpen(){
+	if (isMenuhide ) {
+		
+		sideMenu.style.width ="45vw"
+		sideMenu.style.opacity = 0.7;
+		document.querySelector('#toggle').style.translate ='rotated(90deg)';
+		
+
+		isMenuhide = false;
+
 	}else {
-		document.getElementById('list').style.display = 'none'
-		document.getElementById('line3').style.display="flex"
-		document.getElementById('line1').style.transform= "rotate(-0)"
-		document.getElementById('line2').style.transform= "rotate(0)"
-		document.getElementById('line1').style.marginButtom= "2px"
-		document.getElementById('line2').style.marginTop= "2px"
-		document.getElementById('line2').style.transition= "0.3s"
-		document.getElementById('line1').style.transition= "0.3s"
-		document.getElementById('line1').style.backgroundColor= "white"
-		document.getElementById('line2').style.backgroundColor= "white"
-		isMenuShown = false
+		sideMenu.style.width ="0vw"
+		sideMenu.style.opacity = 0;
+		document.getElementById('line2').style.display ="flex";
+		isMenuhide = true;
 
 	}
-	document.getElementById('toggle').style.animationName= "rotated"
-	
+
 }
 
 
 function hideMenu(){
-	document.getElementById('list').style.display = 'none'
-	document.getElementById('line3').style.display="flex"
-		document.getElementById('line1').style.transform= "rotate(-0)"
-		document.getElementById('line2').style.transform= "rotate(0)"
-		document.getElementById('line1').style.marginButtom= "2px"
-		document.getElementById('line2').style.marginTop= "2px"
-		document.getElementById('line1').style.backgroundColor= "white"
-		document.getElementById('line2').style.backgroundColor= "white"
-		isMenuShown = false
-
-}
-
-function background(){
-	document.getElementById("ab").style.opacity = 1
+		sideMenu.style.width ="0vw"
+		sideMenu.style.opacity = 0;
+		document.getElementById('line2').style.display ="flex";
+		isMenuhide = true;
 }
 
 
 
-
-
-
-
-
-
+function clean(){
+	document.getElementById('email').value ="";
+}
 
 
 
